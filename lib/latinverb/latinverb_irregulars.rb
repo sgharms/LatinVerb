9737e252a45c9318e619f06182c9e060ddf7e22e
Steven G. Harms <sgharms@stevengharms.com>
2011-02-09 18:04:50 -0800

    Base framework for irregulars.

---
+++ lib/latinverb/latinverb_irregulars.rb
@@ -0,0 +1,161 @@
+module Linguistics
+  module Latin
+    module Verb
+      module Irregular
+        SUM_ESSE_FUI_FUTURUS=      
+        {
+          "active_voice_imperative_mood_present_tense": null,
+          "active_voice_indicative_mood_future_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+                "erō",
+                "eris",
+                "erit",
+                "erimus",
+                "eritis",
+                "erunt"
+              ]
+            },
+          "active_voice_indicative_mood_futureperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "active_voice_indicative_mood_imperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+                "eram",
+                "erās",
+                "erat",
+                "eramus",
+                "eratis",
+                "erant"
+              ]
+            },
+          "active_voice_indicative_mood_pastperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "active_voice_indicative_mood_perfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+                "fui",
+                "fuisti",
+                "fuit",
+                "fuimus",
+                "fuistis",
+                "fuerunt"
+              ]
+            },
+          "active_voice_indicative_mood_present_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+                "sum",
+                "es",
+                "est",
+                "sumus",
+                "estis",
+                "sunt"
+              ]
+            },
+          "active_voice_subjunctive_mood_imperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+                "essem",
+                "esses",
+                "esset",
+                "essemus",
+                "essetis",
+                "essent"
+              ]
+            },
+          "active_voice_subjunctive_mood_pastperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+                "amāvissem",
+                "amāvissēs",
+                "amāvisset",
+                "amāvissēmus",
+                "amāvissētis",
+                "amāvissent"
+              ]
+            },
+          "active_voice_subjunctive_mood_perfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+                "amāverim",
+                "amāverīs",
+                "amāverit",
+                "amāverīmus",
+                "amāverītis",
+                "amāverint"
+              ]
+            },
+          "active_voice_subjunctive_mood_present_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+                "amem",
+                "amēs",
+                "amet",
+                "amēmus",
+                "amētis",
+                "ament"
+              ]
+            },
+          "passive_voice_indicative_mood_future_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_indicative_mood_futureperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_indicative_mood_imperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_indicative_mood_pastperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_indicative_mood_perfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_indicative_mood_present_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_subjunctive_mood_imperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_subjunctive_mood_pastperfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_subjunctive_mood_perfect_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            },
+          "passive_voice_subjunctive_mood_present_tense": {
+              "json_class": "Linguistics::Latin::Verb::TenseBlock",
+              "data": [
+              ]
+            }
+        }
+      
+      end
+    end
+  end
+end
