7a60c7641876d5ce8239487c3adcf350f837d1d2
Steven G. Harms <sgharms@stevengharms.com>
2011-03-03 20:13:24 -0800

    Add JSON serialization to LatinVerb class

--- lib/latinverb/latinverb_data.rb
+++ lib/latinverb/latinverb_data.rb
@@ -1,7 +1,12 @@
+require 'json'
+
 module Linguistics
   module Latin
     module Verb
       class LatinVerb
+        def self.json_create(o)
+          new( o )
+        end
         def to_hash
           @tense_list.each do |t|
             ts = t.to_sym
@@ -15,11 +20,47 @@ module Linguistics
             @data_structure.to_yaml
         end
         alias_method :to_y, :to_yaml
-        def to_json
-          @data_structure.empty? ?
-            to_hash.to_json:
-            @data_structure.to_json
+        def to_json(*a)
+          json_hash = {'json_class' => self.class.name}
+          
+          # In the case that we're working with a regular verb, the only thing
+          # we need to save is the original string, since everything can be
+          # re-derived from it easily.
+          unless self.irregular?
+            # While this single string is sufficient to freeze and revivifty
+            # the verb, it means that the JSON structure is rather vacuous.
+            # Given this, the hash is enriched so that the JSON data is
+            # useful.  Nevertheless, in the revivification process, only
+            # 'orig_string' is used.
+            %w{original_string classification stem}.each do |k|
+              json_hash[k] = self.send k.to_sym
+            end
+            # json_hash['original_string']  = original_string
+            # json_hash['classification'] = classification
+            # json_hash['stem'] = stem
+            json_hash['tense_list' ]  = {}
+            @tense_list.each do |t|
+              json_hash['tense_list'][t.to_s] = self.send t.to_sym 
+            end
+            json_hash['irregular']    = irregular?
+            return json_hash.to_json(*a)
+          end
+
+          {
+            'json_class' => self.class.name,
+            'revivify'    => 
+              { 
+                # tense_blocks => JSON string
+                :tense_blocks =>
+                                (@data_structure.empty? ?
+                                  to_hash:
+                                  @data_structure).to_json,
+                :orig_string  => @original_string.to_json,
+
+              }
+           }.to_json(*a)
         end
+
         def pretty_generate
           JSON.pretty_generate(@data_structure.keys.length < 1 ? to_hash : @data_structure)
         end
