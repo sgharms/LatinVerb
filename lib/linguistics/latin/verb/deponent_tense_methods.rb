9da4a10dea382e90b2b99eec8f06c272680b9588
Steven G. Harms <sgharms@stevengharms.com>
2011-05-10 13:51:35 -0700

    Adds missing methods for semi-deponents

--- lib/linguistics/latin/verb/deponent_tense_methods.rb
+++ lib/linguistics/latin/verb/deponent_tense_methods.rb
@@ -69,6 +69,14 @@ module Linguistics
 
         # Subjunctives: 2 active, 2 passive
 
+        def active_voice_subjunctive_mood_present_tense
+          return @proxyVerb.send :active_voice_subjunctive_mood_present_tense
+        end
+
+        def active_voice_subjunctive_mood_imperfect_tense
+          return @proxyVerb.send :active_voice_subjunctive_mood_imperfect_tense
+        end
+
         def active_voice_subjunctive_mood_perfect_tense
           return @proxyVerb.send :passive_voice_subjunctive_mood_perfect_tense
         end
