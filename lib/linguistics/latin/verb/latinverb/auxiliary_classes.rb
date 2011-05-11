439b45538936c108f8556ca67b0335c3581d9c94
Steven G. Harms <sgharms@stevengharms.com>
2011-05-11 08:28:35 -0700

    Adds ParticipleBlock class
    This class functions like an InfinitiveBlock: basically an Array with
    serialization options.

--- lib/linguistics/latin/verb/latinverb/auxiliary_classes.rb
+++ lib/linguistics/latin/verb/latinverb/auxiliary_classes.rb
@@ -6,6 +6,41 @@ require 'yaml'
 module Linguistics 
   module Latin 
     module Verb 
+
+      class ParticipleBlock
+        attr_reader :participle_methods
+
+
+        def initialize(s)
+          raise "ParticipleBlock instantiation argument was nil!" if s.nil?
+
+          if s.class == Hash
+            @participle_methods = s.keys
+          end
+
+          @participle_methods.each do |k|
+            v=s[k]
+            singleton_class.class_eval do
+              define_method k.to_sym do
+                return v 
+              end
+            end
+          end
+        end
+
+        ##
+        #
+        # Required for deserialization
+        #
+        ##
+        def self.json_create(o)
+          new(o['data']) 
+        end
+      end
+
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
       ##
       #
       # The InfinitiveBlock holds the infinitives associated with a given
