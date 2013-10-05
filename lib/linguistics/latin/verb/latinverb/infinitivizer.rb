312a993e1983cbcdb2b3d01c9b1a2007748d3faa
Steven G. Harms <steven.harms@gmail.com>
2013-10-05 11:02:52 -0700

    Update module inclusion to make ctags happy

--- lib/linguistics/latin/verb/latinverb/infinitivizer.rb
+++ lib/linguistics/latin/verb/latinverb/infinitivizer.rb
@@ -1,53 +1,59 @@
-class Linguistics::Latin::Verb::Infinitivizer
-  def initialize(verb)
-    @verb = verb
-  end
+module Linguistics
+  module Latin
+    module Verb
+      class Infinitivizer
+        def initialize(verb)
+          @verb = verb
+        end
 
-  def infinitives
-    return {
-      :present_active_infinitive  => present_active_infinitive,
-      :perfect_active_infinitive  => perfect_active_infinitive,
-      :future_active_infinitive   => future_active_infinitive,
-      :present_passive_infinitive => present_passive_infinitive,
-      :perfect_passive_infinitive => perfect_passive_infinitive,
-      :future_passive_infinitive  => future_passive_infinitive
-    }
-  end
+        def infinitives
+          return {
+            :present_active_infinitive  => present_active_infinitive,
+            :perfect_active_infinitive  => perfect_active_infinitive,
+            :future_active_infinitive   => future_active_infinitive,
+            :present_passive_infinitive => present_passive_infinitive,
+            :perfect_passive_infinitive => perfect_passive_infinitive,
+            :future_passive_infinitive  => future_passive_infinitive
+          }
+        end
 
-  def perfect_active_infinitive
-    @verb.first_person_perfect+"sse"
-  end
+        def perfect_active_infinitive
+          @verb.first_person_perfect+"sse"
+        end
 
-  def future_active_infinitive
-    @verb.future_active_participle.sub(/,.*/,'') + " esse"
-  end
+        def future_active_infinitive
+          @verb.future_active_participle.sub(/,.*/,'') + " esse"
+        end
 
-  def present_passive_infinitive
-    present_active_infinitive = @verb.present_active_infinitive
-    verb_type = @verb.verb_type
+        def present_passive_infinitive
+          present_active_infinitive = @verb.present_active_infinitive
+          verb_type = @verb.verb_type
 
-    if verb_type == Linguistics::Latin::Verb::VerbTypes::First
-      return  present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          if verb_type == Linguistics::Latin::Verb::VerbTypes::First
+            return  present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          end
+          if verb_type == Linguistics::Latin::Verb::VerbTypes::Second
+            return present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          end
+          if verb_type == Linguistics::Latin::Verb::VerbTypes::ThirdIO
+            return present_active_infinitive.gsub(/(.*)ere$/,"\\1ī")
+          end
+          if verb_type == Linguistics::Latin::Verb::VerbTypes::Third
+            return present_active_infinitive.gsub(/(.*)ere$/,"\\1ī")
+          end
+          if verb_type == Linguistics::Latin::Verb::VerbTypes::Fourth
+            return present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          end
+        end
+
+        def perfect_passive_infinitive
+          @verb.perfect_passive_participle + " esse"
+        end
+
+        def future_passive_infinitive
+          @verb.supine[:accusative] + " īrī"
+        end
+      end
     end
-    if verb_type == Linguistics::Latin::Verb::VerbTypes::Second
-      return present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
-    end
-    if verb_type == Linguistics::Latin::Verb::VerbTypes::ThirdIO
-      return present_active_infinitive.gsub(/(.*)ere$/,"\\1ī")
-    end
-    if verb_type == Linguistics::Latin::Verb::VerbTypes::Third
-      return present_active_infinitive.gsub(/(.*)ere$/,"\\1ī")
-    end
-    if verb_type == Linguistics::Latin::Verb::VerbTypes::Fourth
-      return present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
-    end
-  end
-
-  def perfect_passive_infinitive
-    @verb.perfect_passive_participle + " esse"
-  end
-
-  def future_passive_infinitive
-    @verb.supine[:accusative] + " īrī"
   end
 end
