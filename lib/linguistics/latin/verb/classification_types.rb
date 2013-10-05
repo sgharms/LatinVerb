c14a5fffea464359947956dcaba4f28e2a44db04
Steven G. Harms <steven.harms@gmail.com>
2013-10-05 12:19:47 -0700

    Infinitivizer: rm case statement, prefer polymorphism

--- lib/linguistics/latin/verb/classification_types.rb
+++ lib/linguistics/latin/verb/classification_types.rb
@@ -21,12 +21,18 @@ module Linguistics
               [stem, stem+"te"]
             end
           end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+            present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          end
         end
         class Second < VerbType
           def self.form_present_tense_imperative_rule
             ->(stem, ppi) do
               [stem, stem+"te"]
             end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+            present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
           end
         end
         class Third < VerbType
@@ -35,12 +41,18 @@ module Linguistics
               [stem+"e", stem+"ite"]
             end
           end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+             present_active_infinitive.gsub(/(.*)ere$/,"\\1ī")
+          end
         end
         class ThirdIO < VerbType
           def self.form_present_tense_imperative_rule
             ->(stem, ppi) do
               [stem+"e", stem+"ite"]
             end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+             present_active_infinitive.gsub(/(.*)ere$/,"\\1ī")
           end
         end
         class Fourth < VerbType
@@ -49,12 +61,17 @@ module Linguistics
               [stem+"ī", stem+"īte"]
             end
           end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+             present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          end
         end
         class Irregular < VerbType
           def self.form_present_tense_imperative_rule
             ->(stem, ppi) do
               [stem, ppi]
             end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
           end
         end
       end
