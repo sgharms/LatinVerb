f64e9788448f6144691105a7ab58baba271c5cc2
Steven G. Harms <steven.harms@gmail.com>
2013-10-05 14:14:27 -0700

    Move VerbTypes out of ClassificationTypes file

---
+++ lib/linguistics/latin/verb/verb_types.rb
@@ -0,0 +1,68 @@
+module Linguistics
+  module Latin
+    module Verb
+      module VerbTypes
+        class VerbType; end
+        class First < VerbType
+          def self.form_present_tense_imperative_rule
+            ->(stem, ppi) do
+              [stem, stem+"te"]
+            end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+            present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          end
+        end
+        class Second < VerbType
+          def self.form_present_tense_imperative_rule
+            ->(stem, ppi) do
+              [stem, stem+"te"]
+            end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+            present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          end
+        end
+        class Third < VerbType
+          def self.form_present_tense_imperative_rule
+            ->(stem, ppi) do
+              [stem+"e", stem+"ite"]
+            end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+             present_active_infinitive.gsub(/(.*)ere$/,"\\1ī")
+          end
+        end
+        class ThirdIO < VerbType
+          def self.form_present_tense_imperative_rule
+            ->(stem, ppi) do
+              [stem+"e", stem+"ite"]
+            end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+             present_active_infinitive.gsub(/(.*)ere$/,"\\1ī")
+          end
+        end
+        class Fourth < VerbType
+          def self.form_present_tense_imperative_rule
+            ->(stem, ppi) do
+              [stem+"ī", stem+"īte"]
+            end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+             present_active_infinitive.gsub(/(.*)e$/,"\\1ī")
+          end
+        end
+        class Irregular < VerbType
+          def self.form_present_tense_imperative_rule
+            ->(stem, ppi) do
+              [stem, ppi]
+            end
+          end
+          def self.form_present_passive_infinitive(present_active_infinitive)
+          end
+        end
+      end
+    end
+  end
+end
