4c2c2992891d3ff35b8063b564e55d9410f132d8
Steven G. Harms <steven.harms@gmail.com>
2013-10-06 14:16:41 -0700

    Put classification specific mixins in same namespace
    "Impersonal" as a construct or "Irregular" aren't
    features of LatinVerb, but of Latin.  Make naming
    reflect that.

--- lib/latinverb/tense_method_applicator/impersonal.rb
+++ lib/latinverb/tense_method_applicator/impersonal.rb
@@ -1,7 +1,6 @@
 module Linguistics
   module Latin
     module Verb
-      class LatinVerb
         ##
         #
         # Some verbs only take a active/indic/pres/3rd/sg ("it rains").  For
@@ -9,22 +8,21 @@ module Linguistics
         # respond to THAT vector.
         #
         ##
-        module Impersonal
-          def self.extended(extending_instance)
-            extending_instance.instance_eval do
-              prepare_present_only
-            end
+      module Impersonal
+        def self.extended(extending_instance)
+          extending_instance.instance_eval do
+            prepare_present_only
           end
+        end
 
-          def prepare_present_only
-            singleton_class.class_eval do
-              def active_voice_indicative_mood_present_tense
-                TenseBlock.new ["", "", @original_string,
-                                "", "", ""]
-              end
-              def active_voice_indicative_mood_present_tense_third_person_singular_number
-                return active_voice_indicative_mood_present_tense[2]
-              end
+        def prepare_present_only
+          singleton_class.class_eval do
+            def active_voice_indicative_mood_present_tense
+              TenseBlock.new ["", "", @original_string,
+                              "", "", ""]
+            end
+            def active_voice_indicative_mood_present_tense_third_person_singular_number
+              return active_voice_indicative_mood_present_tense[2]
             end
           end
         end
