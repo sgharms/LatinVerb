3e6fbba88bc1ff268ea907e952218706351122c8
Steven G. Harms <sharms@shopittome.com>
2013-10-15 17:36:16 -0700

    rm legacy require to invariant module

--- lib/linguistics/latin/verb/tense_definitions.rb
+++ lib/linguistics/latin/verb/tense_definitions.rb
@@ -1,6 +1,5 @@
 require_relative 'tense_definitions/irregular'
 require_relative 'tense_definitions/impersonal'
-require_relative 'tense_definitions/invariant'
 require_relative 'tense_definitions/first'
 require_relative 'tense_definitions/second'
 require_relative 'tense_definitions/third'
