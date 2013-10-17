8e0076d2b7c16514a81b7cc1f4f37fdcb1559041
Steven G. Harms <steven.harms@gmail.com>
2013-10-17 02:25:21 -0700

    Classifier:Use a more OO style
    Instead of setting an ivar based on a query
    launched form `initialize`, let's set some ivars
    and then write internal methods which change the
    ivars and provide them in teh appropriate
    contexts.
    
    Also, make `classify` a public method.

--- lib/latinverb/components/classifier.rb
+++ lib/latinverb/components/classifier.rb
@@ -1,59 +1,41 @@
+require 'latinverb/components/present_only_checker'
+
 module Linguistics
   module Latin
     module Verb
       class LatinVerb
-        ### {{{
-        #
-        # == ARGUMENTS
-        #
-        #    * s :: a "four principal parts" string whence can be derived
-        #  the first person singular present indicative as well as the
-        #  infinitive
-        #
-        # == RETURNS
-        #
-        # The classification, a subclass of VerbType
-        #
-        # == PURPOSE
-        #
-        # Given the principal parts as a string, decide which conjuation is
-        # in play
-        #
-        #
-        ### }}}
         class LatinVerbClassifier
           attr_reader :classification
 
           def initialize(input)
-            @classification = self.classify input
+            @input = input
           end
 
-          def classify(s)
-            divided_string = s.split( /\s+/ )
-            first_pres = divided_string[0] || ''
-            infinitive = divided_string[1] || ''
-
-            return Linguistics::Latin::Verb::Classification::Defective if
+          def classification
+            return Classification::Defective if
               Linguistics::Latin::Verb::DEFECTIVE_VERBS.member? first_pres
 
-            return Linguistics::Latin::Verb::Classification::Irregular if
+
+            return Classification::Irregular if
               Linguistics::Latin::Verb::IRREGULAR_VERBS.member? first_pres
 
-            return Linguistics::Latin::Verb::Classification::Semideponent if
+            return Classification::Semideponent if
               Linguistics::Latin::Verb::SEMI_DEPONENTS.keys.any?{ |k| first_pres=~/#{k}$/} &&
-              s !~ /PreventDeponentInfiniteRegress/
+              @input !~ /PreventDeponentInfiniteRegress/
 
-            return Linguistics::Latin::Verb::Classification::Impersonal if
-              Linguistics::Latin::Verb::IMPERSONAL_VERBS.member?(s)
+            return Classification::Impersonal if
+              Linguistics::Latin::Verb::IMPERSONAL_VERBS.member?(@input)
 
             # Very irregular irregulars, A&G206, e/f
-            return Linguistics::Latin::Verb::Classification::Irregular if
-              s =~ %r'^(aiō|quaesō|ovāre)$'
+            return Classification::Irregular if
+              @input =~ %r'^(aiō|quaesō|ovāre)$'
 
-            return Linguistics::Latin::Verb::Classification::Deponent if
+            return Classification::PresentOnly if PresentOnlyChecker.new(@input).present_only?
+
+            return Classification::Deponent if
               (infinitive =~ /ī$/ && first_pres =~ /r$/)
 
-            return Linguistics::Latin::Verb::Classification::Regular
+            return Classification::Regular
           end
 
           def set_as_defective
@@ -63,31 +45,41 @@ module Linguistics
           end
 
           def present_only?
-            @present_only ||= ( @classification ==  Linguistics::Latin::Verb::Classification::PresentOnly )
+            classification == Linguistics::Latin::Verb::Classification::PresentOnly
           end
 
           def deponent?
-            @deponent ||= (@classification == Linguistics::Latin::Verb::Classification::Deponent)
+            classification == Linguistics::Latin::Verb::Classification::Deponent
           end
 
           def semideponent?
-            @semideponent ||= (@classification == Linguistics::Latin::Verb::Classification::Semideponent)
+            classification == Linguistics::Latin::Verb::Classification::Semideponent
           end
 
           def impersonal?
-            @impersonal ||= (@classification == Linguistics::Latin::Verb::Classification::Impersonal)
+            classification == Linguistics::Latin::Verb::Classification::Impersonal
           end
 
           def irregular?
-            @irregular = @classification == Linguistics::Latin::Verb::Classification::Irregular
+            classification == Linguistics::Latin::Verb::Classification::Irregular
           end
 
           def regular?
-            @regular = @classification == Linguistics::Latin::Verb::Classification::Regular
+            classification == Linguistics::Latin::Verb::Classification::Regular
           end
 
           def short_class
-            @classification.to_s.gsub(/.*::(\w+)$/,"\\1")
+            classification.to_s.gsub(/.*::(\w+)$/,"\\1")
+          end
+
+          private
+
+          def first_pres
+            @input.split(/\s+/)[0]
+          end
+
+          def infinitive
+            @input.split(/\s+/)[1]
           end
 
         end
