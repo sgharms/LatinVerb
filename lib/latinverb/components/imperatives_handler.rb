754f0ca806dc84fb391f6e742bf2c8f0b294490e
Steven G. Harms <steven.harms@gmail.com>
2013-10-18 02:44:50 -0700

    InfinitivesHandler: use Forwardable, extract methods

--- lib/latinverb/components/imperatives_handler.rb
+++ lib/latinverb/components/imperatives_handler.rb
@@ -3,6 +3,10 @@ module Linguistics
     module Verb
       class LatinVerb
         class ImperativesHandler
+          extend Forwardable
+
+          def_delegators :@verb, :stem, :present_active_infinitive
+
           IMPERATIVE_EXCEPTIONS = {
             "ducere"   => %w(duc ducite),
             "dicere"   => %w(dic dicite),
@@ -16,18 +20,18 @@ module Linguistics
           end
 
           def imperatives
-            args = exceptional_imperative? ? calculate_exceptional_imperatives : [@verb.stem, @verb.present_active_infinitive]
+            args = exceptional_imperative? ? calculate_exceptional_imperatives : [stem, present_active_infinitive]
             Linguistics::Latin::Verb::ImperativeBlock.new(*args, @verb)
           end
 
           private
 
           def exceptional_imperative?
-            IMPERATIVE_EXCEPTIONS.has_key?(@verb.present_active_infinitive)
+            IMPERATIVE_EXCEPTIONS.has_key?(present_active_infinitive)
           end
 
           def calculate_exceptional_imperatives
-            IMPERATIVE_EXCEPTIONS[@verb.present_active_infinitive]
+            IMPERATIVE_EXCEPTIONS[present_active_infinitive]
           end
         end
       end
