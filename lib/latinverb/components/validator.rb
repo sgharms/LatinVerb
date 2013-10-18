3486123af7291aa549ce5c3c00df9addc9b55a45
Steven G. Harms <steven.harms@gmail.com>
2013-10-18 02:19:06 -0700

    Validator: Remove comments
    Out of date.

--- lib/latinverb/components/validator.rb
+++ lib/latinverb/components/validator.rb
@@ -2,33 +2,7 @@ module Linguistics
   module Latin
     module Verb
       class LatinVerb
-        ##
-        # == NAME
-        #
-        # Validation
-        #
-        # == DESCRIPTION
-        #
-        # This module contains the validity testing methods, when mixed into a
-        # LatinVerb, will provide it the ability to ensure its own sanity.
-        #
-        ##
         class Validator
-          ##
-          #
-          # == DESCRIPTION
-          #
-          # This performs the basic task of evaluating the given string (cf.
-          # LatinVerb.initialize) for basic sanity.
-          #
-          # Here are its basic truths
-          #
-          # 1.  Get +@original_string+ as an iVar
-          # 1.  Derive a classification (+@classification+) from the string
-          # 1.  Determine whether the string qualifies the verb as irregular
-          # 1.  Identify a stem (+@stem+), provided the verb is regular
-          #
-          ##
           def initialize(verb)
             @verb = verb
           end
@@ -51,7 +25,6 @@ module Linguistics
             return true if @verb.irregular?
             !@verb.stem.nil?
           end
-
         end
       end
     end
