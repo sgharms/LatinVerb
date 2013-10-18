0d204af09d8c2ecdca02f3a43a1d5229f5df0f82
Steven G. Harms <steven.harms@gmail.com>
2013-10-18 02:43:08 -0700

    Infinitivizer: use Forwardable, extract methods

--- lib/latinverb/components/infinitivizer.rb
+++ lib/latinverb/components/infinitivizer.rb
@@ -3,6 +3,10 @@ module Linguistics
     module Verb
       class LatinVerb
         class Infinitivizer
+          extend Forwardable
+
+          def_delegators :@verb, :first_person_perfect, :future_active_participle, :verb_type, :present_active_infinitive, :perfect_passive_participle, :supine
+
           def initialize(verb)
             @verb = verb
           end
@@ -19,25 +23,23 @@ module Linguistics
           end
 
           def perfect_active_infinitive
-            @verb.first_person_perfect + "sse"
+            first_person_perfect + "sse"
           end
 
           def future_active_infinitive
-            @verb.future_active_participle.sub(/,.*/,'') + " esse"
+            future_active_participle.sub(/,.*/,'') + " esse"
           end
 
           def present_passive_infinitive
-            verb_type = @verb.verb_type
-            present_active_infinitive = @verb.present_active_infinitive
             verb_type.form_present_passive_infinitive(present_active_infinitive)
           end
 
           def perfect_passive_infinitive
-            @verb.perfect_passive_participle + " esse"
+            perfect_passive_participle + " esse"
           end
 
           def future_passive_infinitive
-            @verb.supine[:accusative] + " īrī"
+            supine[:accusative] + " īrī"
           end
         end
       end
