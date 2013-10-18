de8a0c34c27870e4c56161bbb29a6c54e8a6c64c
Steven G. Harms <steven.harms@gmail.com>
2013-10-18 01:41:06 -0700

    migrate stem calculation out of prin parts extractor

--- lib/latinverb.rb
+++ lib/latinverb.rb
@@ -25,9 +25,10 @@ module Linguistics
 
         def_delegators :@validator, :valid?
         def_delegators :@classifier, :classification, :irregular?, :present_only?, :regular?, :set_as_defective, :short_class
-        def_delegators :@prin_parts_extractor, :first_person_perfect, :first_person_perfect, :first_person_singular, :participial_stem, :passive_perfect_participle, :present_active_infinitive, :present_active_infinitive, :principal_parts, :stem 
+        def_delegators :@prin_parts_extractor, :first_person_perfect, :first_person_perfect, :first_person_singular, :passive_perfect_participle, :present_active_infinitive, :present_active_infinitive, :principal_parts
         def_delegators :@participler, :supine, :future_active_participle, :future_passive_participle, :gerund, :gerundive, :perfect_passive_participle, :present_active_participle
         def_delegators :@infinitivizer, :future_active_infinitive, :future_passive_infinitive, :infinitives, :perfect_active_infinitive, :perfect_passive_infinitive, :present_passive_infinitive
+        def_delegators :@stem_deriver, :stem, :participial_stem
         def_delegators :@chart_presenter, :chart, :c
         def_delegator  :@imperative_handler, :imperatives
 
@@ -66,6 +67,7 @@ module Linguistics
           @classifier = LatinVerbClassifier.new(@original_string)
           @prin_parts_extractor = LatinVerbPrincipalPartsExtractor.new(@original_string, @classifier)
           @stem_deriver = LatinverbStemDeriver.new(self)
+          @verb_type = LatinVerbTypeEvaluator.new(first_person_singular, present_active_infinitive, @classifier, self).inspect
         end
 
         def build_validator
--- lib/latinverb/components/principal_parts_extractor.rb
+++ lib/latinverb/components/principal_parts_extractor.rb
@@ -6,14 +6,6 @@ module Linguistics
           def initialize(input_string, classification)
             @data_string = input_string
             @classification = classification
-
-            if deponent_or_impersonal?
-                @deponent_proxy = Linguistics::Latin::Verb::LatinVerb::DeponentStringDeriver.new(input_string).proxy_string
-            else
-              @principal_parts = principal_parts
-              @first_person_singular, @present_active_infinitive,
-              @first_person_perfect, @passive_perfect_participle = @principal_parts
-            end
           end
 
           def principal_parts
@@ -36,66 +28,6 @@ module Linguistics
             principal_parts[3]
           end
 
-          def participial_stem
-            calculate_participial_stem(present_active_infinitive, first_person_singular)
-          end
-
-          def stem
-            if @classification.regular?
-              calculate_stem(present_active_infinitive, first_person_singular)
-            elsif @deponent_proxy
-              DeponentStringDeriver.new(@data_string).proxy_string.split(/\s+/)[1]
-            end
-          end
-
-          private
-
-          def deponent_or_impersonal?
-            @classification.deponent? or @classification.semideponent? or @classification.impersonal?
-          end
-
-          def calculate_stem(present_active_infinitive, first_person_singular)
-            if present_active_infinitive =~ /āre$/
-              return present_active_infinitive.gsub(/(.*)āre$/,'\\1ā')
-            end
-            if present_active_infinitive =~ /ēre$/
-              return present_active_infinitive.gsub(/(.*)ēre$/,'\\1ē')
-            end
-            if present_active_infinitive =~ /ere$/
-              if first_person_singular =~ /iō$/
-                return present_active_infinitive.gsub(/(.*)ere$/,'\\1')
-              else
-                return present_active_infinitive.gsub(/(.*)ere$/,'\\1')
-              end
-            end
-            if present_active_infinitive =~ /īre$/
-              return present_active_infinitive.gsub(/(.*)īre$/,'\\1')
-            end
-          end
-
-
-          def calculate_participial_stem(present_active_infinitive, first_person_singular)
-            if present_active_infinitive.to_s =~ /(.*ā)re$/
-              return $1
-            end
-
-            if present_active_infinitive.to_s =~ /(.*ē)re$/
-              return $1
-            end
-
-            if present_active_infinitive.to_s =~ /(.*)ere$/
-              match=$1
-              if first_person_singular =~ /iō/
-                return match + "iē"
-              else
-                return match + "e"
-              end
-            end
-
-            if present_active_infinitive.to_s =~ /(.*)īre$/
-              return $1 + "iē"
-            end
-          end
         end
       end
     end
