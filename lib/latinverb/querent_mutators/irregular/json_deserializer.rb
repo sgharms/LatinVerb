aa01c1519281a796e353729d94626c335a3e3e7a
Steven G. Harms <steven.harms@gmail.com>
2013-11-21 11:09:17 -0800

    Correct Mutators -> QuerentMutators naming

--- lib/latinverb/querent_mutators/irregular/json_deserializer.rb
+++ lib/latinverb/querent_mutators/irregular/json_deserializer.rb
@@ -2,7 +2,7 @@ module Linguistics
   module Latin
     module Verb
       class LatinVerb
-        module Mutators
+        module QuerentMutators
           class Irregular
             class Deserializer
               attr_reader :revivified_data_structure
