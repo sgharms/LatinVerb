4b1df8f6163f0aa9d5bc8a50b83ae368fc9ad568
Steven G. Harms <steven.harms@gmail.com>
2013-11-21 10:52:02 -0800

    rm duplicated class definition

--- lib/latinverb/querent_mutators/irregular.rb
+++ lib/latinverb/querent_mutators/irregular.rb
@@ -8,42 +8,6 @@ module Linguistics
       class LatinVerb
         module QuerentMutators
           class Irregular
-            class Revivifier
-              def initialize(verb, structure)
-                @verb = verb
-                @structure = structure
-                byebug
-                build_tense_blocks!
-                build_infinitives!
-                build_participles!
-              end
-
-              private
-
-              def build_tense_blocks!
-                #NEU!
-                @structure['tense_blocks'].each_pair do |tense_block_name, serialized_tense_block|
-                  @verb.singleton_class.class_eval do
-                    define_method tense_block_name.to_sym do
-                      TenseBlock.new serialized_tense_block["data"], { :meaning => MEANINGS[tense_block_name.to_sym] }
-                    end
-                  end
-                end
-              end
-
-              def build_infinitives!
-                irregular_infinitives = @structure['infinitives']['data']
-                return if irregular_infinitives.nil?
-                InfinitivesBuilder.new(@verb, irregular_infinitives)
-              end
-
-              def build_participles!
-                irregular_participles = @structure['participles']['data']
-                return if irregular_participles.nil?
-                ParticiplesBuilder.new(@verb, irregular_participles)
-              end
-            end
-
             def initialize(verb)
               byebug; #neu
               @verb = verb
