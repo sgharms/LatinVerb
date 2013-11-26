6cd70d3251cc773ac0c479fa0b7c77d7420db543
Steven G. Harms <steven.harms@gmail.com>
2013-11-25 18:00:33 -0800

    wip

--- lib/latinverb.rb
+++ lib/latinverb.rb
@@ -80,7 +80,7 @@ module Linguistics
 
         def build_lookup_components!
           @querent = QuerentForClassificationBuilder.new(self).querent
-          @infinitivizer, @imperative_handler, @participler = irregular? ? components_for_irregular : components
+          @infinitivizer, @imperative_handler, @participler = ComponentsFactory.new(self).components
         end
 
         def apply_chart_capabilities!
