5385204207dde4d10f62ddbb304a4d24e10c1876
Steven G. Harms <sharms@shopittome.com>
2013-12-27 14:58:32 -0800

    Formatting

--- lib/latinverb/perfect_tense_remover.rb
+++ lib/latinverb/perfect_tense_remover.rb
@@ -3,18 +3,18 @@ module Linguistics
     module Verb
       class LatinVerb
         class QuerentPerfectTenseRemover
-	  TENSE_BLOCKS_TO_ECLIPSE = [
-	    :active_voice_indicative_mood_futureperfect_tense,
-	    :active_voice_indicative_mood_pastperfect_tense,
-	    :active_voice_indicative_mood_perfect_tense,
-	    :active_voice_subjunctive_mood_pastperfect_tense,
-	    :active_voice_subjunctive_mood_perfect_tense,
-	    :passive_voice_indicative_mood_futureperfect_tense,
-	    :passive_voice_indicative_mood_pastperfect_tense,
-	    :passive_voice_indicative_mood_perfect_tense,
-	    :passive_voice_subjunctive_mood_pastperfect_tense,
-	    :passive_voice_subjunctive_mood_perfect_tense
-	  ]
+          TENSE_BLOCKS_TO_ECLIPSE = [
+            :active_voice_indicative_mood_futureperfect_tense,
+            :active_voice_indicative_mood_pastperfect_tense,
+            :active_voice_indicative_mood_perfect_tense,
+            :active_voice_subjunctive_mood_pastperfect_tense,
+            :active_voice_subjunctive_mood_perfect_tense,
+            :passive_voice_indicative_mood_futureperfect_tense,
+            :passive_voice_indicative_mood_pastperfect_tense,
+            :passive_voice_indicative_mood_perfect_tense,
+            :passive_voice_subjunctive_mood_pastperfect_tense,
+            :passive_voice_subjunctive_mood_perfect_tense
+          ]
 
           def initialize(perfect_tense_block_bearer)
             @perfect_tense_block_bearer = perfect_tense_block_bearer
