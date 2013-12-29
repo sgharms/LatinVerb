de018252fe0cbde0875f9e65c24c072e24a5a732
Steven G. Harms <steven.harms@gmail.com>
2013-12-29 08:58:12 -0800

    rm class_eval in Deponent::TenseBlockMutator

--- lib/latinverb/querent_for_classification_strategy/deponent.rb
+++ lib/latinverb/querent_for_classification_strategy/deponent.rb
@@ -10,8 +10,28 @@ module Linguistics
 
               def initialize(verb)
                 super
-                @proxyVerb = LatinVerb.new(proxy_string)
-                QuerentMutators::Deponent.new(@verb, @querent).mutate!
+		prepare_deponent_proxy!
+                reroute_active_calls_to_proxys_passives!
+              end
+
+	      private
+
+	      def prepare_deponent_proxy!
+		@querent.extend Forwardable
+		@querent.instance_variable_set(:@deponentProxy, LatinVerb.new(proxy_string))
+	      end
+
+              def reroute_active_calls_to_proxys_passives!
+                passive_and_active_pairs do |passive_method_name, active_method_name|
+                  @querent.def_delegator "@deponentProxy", passive_method_name, active_method_name
+                end
+              end
+
+              def passive_and_active_pairs
+                LatinVerb::Querent::PASSIVE_TENSE_METHODS.map do |passive_method_name|
+                  active_correlate = passive_method_name.to_s.sub(/^passive(.*)/, "active\\1")
+                  yield passive_method_name, active_correlate
+                end
               end
 
               def proxy_string
