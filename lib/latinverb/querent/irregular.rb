557e834b4229d0c93a4f98801d7fe08047d2d5a6
Steven G. Harms <steven.harms@gmail.com>
2013-12-29 18:39:56 -0800

    Only one more eval left!

--- lib/latinverb/querent/irregular.rb
+++ lib/latinverb/querent/irregular.rb
@@ -3,13 +3,114 @@ module Linguistics
     module Verb
       class LatinVerb
         class IrregularQuerent < Querent
-          def initialize(*args)
-            @verb = args[0] if !args.empty?
+          def initialize(deserialized_structure)
+            @structure = OpenStruct.new(deserialized_structure)
             @added_vectorized_method = []
           end
 
           def imperatives
             OpenStruct.new( :future => Proc.new{}, :present => Proc.new{} )
+          end
+
+          def active_voice_imperative_mood_future_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_imperative_mood_present_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_indicative_mood_future_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_indicative_mood_futureperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_indicative_mood_imperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_indicative_mood_pastperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_indicative_mood_perfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_indicative_mood_present_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_subjunctive_mood_imperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_subjunctive_mood_pastperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_subjunctive_mood_perfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def active_voice_subjunctive_mood_present_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_indicative_mood_future_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_indicative_mood_futureperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_indicative_mood_imperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_indicative_mood_pastperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_indicative_mood_perfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_indicative_mood_present_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_subjunctive_mood_imperfect_tense
+            deserialized_tense_block_accessor(__method__)
+
+          end
+          def passive_voice_subjunctive_mood_pastperfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_subjunctive_mood_perfect_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+          def passive_voice_subjunctive_mood_present_tense
+            deserialized_tense_block_accessor(__method__)
+          end
+
+
+          private
+
+          def tense_blocks
+            @tense_blocks ||= OpenStruct.new(@structure["tense_blocks"])
+          end
+            
+          def deserialized_tense_block_accessor(block_name)
+            serialized_tense_block = tense_blocks.send(block_name)["data"]
+            opts = {:meaning => MEANINGS[block_name.to_sym]}
+            TenseBlock.new(serialized_tense_block, opts)
           end
         end
       end
