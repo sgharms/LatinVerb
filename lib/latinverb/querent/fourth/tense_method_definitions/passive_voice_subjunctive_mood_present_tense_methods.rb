aa92d1dc8473738e9ef24493b504b75d5625692c
Steven G. Harms <steven.harms@gmail.com>
2014-01-06 23:19:56 -0800

    Fourth Querent: Push tense method definitions down into mixin

---
+++ lib/latinverb/querent/fourth/tense_method_definitions/passive_voice_subjunctive_mood_present_tense_methods.rb
@@ -0,0 +1,94 @@
+module Linguistics
+  module Latin
+    module Verb
+      class LatinVerb
+        class Querent
+          class Fourth
+            module PassiveVoiceSubjunctiveMoodPresentTenseMethods
+              def passive_voice_subjunctive_mood_present_tense
+                subjunctive_stem = stem + "iā"
+                elems = PASSIVE_ENDINGS_FIRST_AND_SECOND_CONJG.map do |ending|
+                  subjunctive_stem + ending
+                end
+                TenseBlock.new(
+                  elems,
+                  { :meaning => MEANINGS[:passive_voice_subjunctive_mood_present_tense] }
+                )
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_first_person_singular_number
+                passive_voice_subjunctive_mood_present_tense.first_person_singular_number
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_singular_number_first_person
+                passive_voice_subjunctive_mood_present_tense.singular_number_first_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_second_person_singular_number
+                passive_voice_subjunctive_mood_present_tense.second_person_singular_number
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_singular_number_second_person
+                passive_voice_subjunctive_mood_present_tense.singular_number_second_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_third_person_singular_number
+                passive_voice_subjunctive_mood_present_tense.third_person_singular_number
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_singular_number_third_person
+                passive_voice_subjunctive_mood_present_tense.singular_number_third_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_first_person_plural_number
+                passive_voice_subjunctive_mood_present_tense.first_person_plural_number
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_plural_number_first_person
+                passive_voice_subjunctive_mood_present_tense.plural_number_first_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_second_person_plural_number
+                passive_voice_subjunctive_mood_present_tense.second_person_plural_number
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_plural_number_second_person
+                passive_voice_subjunctive_mood_present_tense.plural_number_second_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_third_person_plural_number
+                passive_voice_subjunctive_mood_present_tense.third_person_plural_number
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_plural_number_third_person
+                passive_voice_subjunctive_mood_present_tense.plural_number_third_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_first_person
+                passive_voice_subjunctive_mood_present_tense.first_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_second_person
+                passive_voice_subjunctive_mood_present_tense.second_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_third_person
+                passive_voice_subjunctive_mood_present_tense.third_person
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_singular_number
+                passive_voice_subjunctive_mood_present_tense.singular_number
+              end
+
+              def passive_voice_subjunctive_mood_present_tense_plural_number
+                passive_voice_subjunctive_mood_present_tense.plural_number
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+
