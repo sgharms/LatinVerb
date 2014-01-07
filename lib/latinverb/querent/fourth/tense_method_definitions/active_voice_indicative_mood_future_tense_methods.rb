e0fbea6509f7caf9b91dfd647c3e11c36a763ac2
Steven G. Harms <steven.harms@gmail.com>
2014-01-06 23:19:56 -0800

    Fourth Querent: Push tense method definitions down into mixin

---
+++ lib/latinverb/querent/fourth/tense_method_definitions/active_voice_indicative_mood_future_tense_methods.rb
@@ -0,0 +1,90 @@
+module Linguistics
+  module Latin
+    module Verb
+      class LatinVerb
+        class Querent
+          class Fourth
+            module ActiveVoiceIndicativeMoodFutureTenseMethods
+              def active_voice_indicative_mood_future_tense
+                return TenseBlock.new(
+                  [AF_OTHER_ENDINGS.collect{|x| stem + "i" + x}].flatten,
+                  { :meaning => MEANINGS[:active_voice_indicative_mood_future_tense] }
+                )
+              end
+
+              def active_voice_indicative_mood_future_tense_first_person_singular_number
+                active_voice_indicative_mood_future_tense.first_person_singular_number
+              end
+
+              def active_voice_indicative_mood_future_tense_singular_number_first_person
+                active_voice_indicative_mood_future_tense.singular_number_first_person
+              end
+
+              def active_voice_indicative_mood_future_tense_second_person_singular_number
+                active_voice_indicative_mood_future_tense.second_person_singular_number
+              end
+
+              def active_voice_indicative_mood_future_tense_singular_number_second_person
+                active_voice_indicative_mood_future_tense.singular_number_second_person
+              end
+
+              def active_voice_indicative_mood_future_tense_third_person_singular_number
+                active_voice_indicative_mood_future_tense.third_person_singular_number
+              end
+
+              def active_voice_indicative_mood_future_tense_singular_number_third_person
+                active_voice_indicative_mood_future_tense.singular_number_third_person
+              end
+
+              def active_voice_indicative_mood_future_tense_first_person_plural_number
+                active_voice_indicative_mood_future_tense.first_person_plural_number
+              end
+
+              def active_voice_indicative_mood_future_tense_plural_number_first_person
+                active_voice_indicative_mood_future_tense.plural_number_first_person
+              end
+
+              def active_voice_indicative_mood_future_tense_second_person_plural_number
+                active_voice_indicative_mood_future_tense.second_person_plural_number
+              end
+
+              def active_voice_indicative_mood_future_tense_plural_number_second_person
+                active_voice_indicative_mood_future_tense.plural_number_second_person
+              end
+
+              def active_voice_indicative_mood_future_tense_third_person_plural_number
+                active_voice_indicative_mood_future_tense.third_person_plural_number
+              end
+
+              def active_voice_indicative_mood_future_tense_plural_number_third_person
+                active_voice_indicative_mood_future_tense.plural_number_third_person
+              end
+
+              def active_voice_indicative_mood_future_tense_first_person
+                active_voice_indicative_mood_future_tense.first_person
+              end
+
+              def active_voice_indicative_mood_future_tense_second_person
+                active_voice_indicative_mood_future_tense.second_person
+              end
+
+              def active_voice_indicative_mood_future_tense_third_person
+                active_voice_indicative_mood_future_tense.third_person
+              end
+
+              def active_voice_indicative_mood_future_tense_singular_number
+                active_voice_indicative_mood_future_tense.singular_number
+              end
+
+              def active_voice_indicative_mood_future_tense_plural_number
+                active_voice_indicative_mood_future_tense.plural_number
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+
