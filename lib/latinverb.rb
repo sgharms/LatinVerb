2c6a5606392269b08d727b26aee7964eeb18a218
Steven G. Harms <steven.harms@gmail.com>
2014-07-14 06:39:08 -0700

    Push down implementation into latinverb/latinverb

--- lib/latinverb.rb
+++ lib/latinverb.rb
@@ -4,6 +4,7 @@ require 'yaml'
 
 require 'linguistics_latin'
 
+require 'latinverb/latinverb'
 require 'latinverb/errors'
 require 'latinverb/components/chart_presenter'
 require 'latinverb/components/deponent_string_deriver'
@@ -157,78 +158,13 @@ require 'latinverb/serialization/yaml'
 require 'latinverb/serialization/hash'
 require 'latinverb/tense_block/vector_resolution_delegate'
 require 'latinverb/tense_block/null_tense_block'
+require 'latinverb/paradigmatic_verbs'
 
 module Linguistics
   module Latin
     module Verb
       class LatinVerb
-        extend Forwardable
-
-        def_delegators :@validator, :valid?
-        def_delegators :@classifier, :classification, :irregular?, :present_only?, :regular?, :set_as_defective, :short_class, :deponent?, :semideponent?, :proxy_verb?
-        def_delegators :@prin_parts_extractor, :first_person_perfect, :first_person_perfect, :first_person_singular, :passive_perfect_participle, :present_active_infinitive, :present_active_infinitive, :principal_parts
-        def_delegators :@participler, :supine, :future_active_participle, :future_passive_participle, :gerund, :gerundive, :perfect_passive_participle, :present_active_participle, :perfect_active_participle, :participle_methods
-        def_delegators :@infinitivizer, :future_active_infinitive, :future_passive_infinitive, :infinitives, :perfect_active_infinitive, :perfect_passive_infinitive, :present_passive_infinitive, :infinitive_methods
-        def_delegators :@stem_deriver, :stem, :participial_stem
-        def_delegators :@chart_presenter, :chart, :c
-        def_delegators :@type_evaluator, :short_type
-        def_delegator  :@imperative_handler, :imperatives
-
-        def_delegator :@classifier, :to_s, :conjugation
-        def_delegator :@classifier, :dup, :classified_as
-        def_delegator :@type_evaluator, :type, :verb_type
-
-        attr_reader :original_string, :verb_methods, :classifier, :querent, :options
-
-        def initialize(data, options={})
-          @options = options
-          classify(data)
-          build_lookup_components!
-          build_validator!
-          apply_chart_capabilities!
-        end
-
-        def to_s
-          sprintf("%s [%s]", short_class, original_string)
-        end
-
-        def display
-          pretty_generate
-        end
-
-        private
-
-        def classify(data)
-          @original_string = (data['original_string'] || data)
-          @classifier = LatinVerbClassifier.new(self)
-          @prin_parts_extractor = LatinVerbPrincipalPartsExtractor.new(@original_string, @classifier)
-          @stem_deriver = LatinverbStemDeriver.new(self)
-          @type_evaluator = LatinVerbTypeEvaluator.new(self)
-        end
-
-        def build_lookup_components!
-          @querent = QuerentForClassificationBuilder.new(self).querent
-          @infinitivizer, @imperative_handler, @participler = ComponentsFactory.new(self).components
-          delegate_verb_method_calls_to_delegate!
-        end
-
-        def apply_chart_capabilities!
-          @chart_presenter = ChartPresenter.new(self)
-        end
-
-        def build_validator!
-          @validator = Validator.new(self)
-        end
-
-        def delegate_verb_method_calls_to_delegate!
-          self.extend Forwardable
-          @querent.defined_tense_methods.each do |sym|
-            self.def_delegator "@querent", sym
-          end
-        end
       end
     end
   end
 end
-
-require 'latinverb/paradigmatic_verbs'
---
+++ lib/latinverb/latinverb.rb
@@ -0,0 +1,72 @@
+module Linguistics
+  module Latin
+    module Verb
+      class LatinVerb
+        extend Forwardable
+
+        def_delegators :@validator, :valid?
+        def_delegators :@classifier, :classification, :irregular?, :present_only?, :regular?, :set_as_defective, :short_class, :deponent?, :semideponent?, :proxy_verb?
+        def_delegators :@prin_parts_extractor, :first_person_perfect, :first_person_perfect, :first_person_singular, :passive_perfect_participle, :present_active_infinitive, :present_active_infinitive, :principal_parts
+        def_delegators :@participler, :supine, :future_active_participle, :future_passive_participle, :gerund, :gerundive, :perfect_passive_participle, :present_active_participle, :perfect_active_participle, :participle_methods
+        def_delegators :@infinitivizer, :future_active_infinitive, :future_passive_infinitive, :infinitives, :perfect_active_infinitive, :perfect_passive_infinitive, :present_passive_infinitive, :infinitive_methods
+        def_delegators :@stem_deriver, :stem, :participial_stem
+        def_delegators :@chart_presenter, :chart, :c
+        def_delegators :@type_evaluator, :short_type
+        def_delegator  :@imperative_handler, :imperatives
+
+        def_delegator :@classifier, :to_s, :conjugation
+        def_delegator :@classifier, :dup, :classified_as
+        def_delegator :@type_evaluator, :type, :verb_type
+
+        attr_reader :original_string, :verb_methods, :classifier, :querent, :options
+
+        def initialize(data, options={})
+          @options = options
+          classify(data)
+          build_lookup_components!
+          build_validator!
+          apply_chart_capabilities!
+        end
+
+        def to_s
+          sprintf("%s [%s]", short_class, original_string)
+        end
+
+        def display
+          JSON.pretty_generate(to_h)
+        end
+
+        private
+
+        def classify(data)
+          @original_string = (data['original_string'] || data)
+          @classifier = Classifier.new(self)
+          @prin_parts_extractor = LatinVerbPrincipalPartsExtractor.new(@original_string, @classifier)
+          @stem_deriver = LatinverbStemDeriver.new(self)
+          @type_evaluator = LatinVerbTypeEvaluator.new(self)
+        end
+
+        def build_lookup_components!
+          @querent = QuerentForClassificationBuilder.new(self).querent
+          @infinitivizer, @imperative_handler, @participler = ComponentsFactory.new(self).components
+          delegate_verb_method_calls_to_delegate!
+        end
+
+        def apply_chart_capabilities!
+          @chart_presenter = ChartPresenter.new(self)
+        end
+
+        def build_validator!
+          @validator = Validator.new(self)
+        end
+
+        def delegate_verb_method_calls_to_delegate!
+          self.extend Forwardable
+          @querent.defined_tense_methods.each do |sym|
+            self.def_delegator "@querent", sym
+          end
+        end
+      end
+    end
+  end
+end
