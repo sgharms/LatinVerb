5f27b4319aba89685db56454876dca7c3cc9198b
Steven G. Harms <steven.harms@gmail.com>
2014-07-15 14:58:10 -0700

    DeponentImperativesHandler: rm dependency on LatinVerb
    Should make this component more modular.
    Implementation is still a bit hacky.

--- lib/latinverb/components/deponent_imperatives_handler.rb
+++ lib/latinverb/components/deponent_imperatives_handler.rb
@@ -7,22 +7,17 @@ module Linguistics
           def_delegators :@proxyVerb, :stem, :present_active_infinitive
           def_delegators :@verb, :original_string
 
-          def initialize(verb)
-            super
-            @proxyVerb = LatinVerb.new(DeponentStringDeriver.new(original_string).proxy_string, :proxy_verb => true)
-          end
-
           def imperatives
             args = [stem, present_active_infinitive]
             Linguistics::Latin::Verb::ImperativeBlock.new(*args, @verb)
           end
 
           def stem
-            @proxyVerb.stem
+            Linguistics::Latin::Verb::LatinVerb::StemDeriver.new(@verb).stem[0..-3]
           end
 
           def present_active_infinitive
-            @proxyVerb.present_active_infinitive
+            Linguistics::Latin::Verb::LatinVerb::DeponentStringDeriver.new(original_string).proxy_string.split(/\s+/)[1]
           end
         end
       end
