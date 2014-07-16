a25e27c6e6e36f1c56fdf480c250d9a7a9ee8316
Steven G. Harms <steven.harms@gmail.com>
2014-07-16 10:38:11 -0700

    Formatting

--- lib/latinverb/querent/impersonal_verb_mixin.rb
+++ lib/latinverb/querent/impersonal_verb_mixin.rb
@@ -2,11 +2,11 @@ module Linguistics
   module Latin
     module Verb
       class LatinVerb
-	module ImpersonalVerbMixin
-	  def active_voice_indicative_mood_present_tense
-	    TenseBlock.new ["", "", @verb.original_string, "", "", ""]
-	  end
-	end
+        module ImpersonalVerbMixin
+          def active_voice_indicative_mood_present_tense
+            TenseBlock.new ["", "", @verb.original_string, "", "", ""]
+          end
+        end
       end
     end
   end
