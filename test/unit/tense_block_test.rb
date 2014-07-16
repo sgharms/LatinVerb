bbcf5047379810064f58104f91230b12178db6d9
Steven G. Harms <steven.harms@gmail.com>
2014-07-16 15:13:30 -0700

    rm test/unit: pref. integration tests
    LatinVerb has many integration tests, but few unit
    tests. These should be implemented in the
    gem-based components.

--- test/unit/tense_block_test.rb
+++
@@ -1,6 +0,0 @@
-class TenseBlockTest < Minitest::Test
-  def test_eachable
-    stub = Linguistics::Latin::Verb::TenseBlock.new(%w/foo bar bat bin bang bob beetle/)
-    assert_respond_to stub, :each, "Must respond to #each"
-  end
-end
