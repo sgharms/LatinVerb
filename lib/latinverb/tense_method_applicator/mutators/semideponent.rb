7b70e9695f89f8cc25000f35305e5fb90985feb3
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 07:23:28 -0700

    Add null Mutators::Semideponent

---
+++ lib/latinverb/tense_method_applicator/mutators/semideponent.rb
@@ -0,0 +1,18 @@
+module Linguistics
+  module Latin
+    module Verb
+      class LatinVerb
+        module Mutators
+          class Semideponent
+            def initialize(verb)
+            end
+
+            def mutate!
+            end
+
+          end
+        end
+      end
+    end
+  end
+end
