11da14b60f131a06a1dcb5c993068cf9665ca1de
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 04:04:23 -0700

    rm PresentOnlyIrregularMask

--- lib/latinverb/querent_mutators/irregular.rb
+++ lib/latinverb/querent_mutators/irregular.rb
@@ -1,6 +1,5 @@
 require_relative 'irregular/json_deserializer'
 require_relative 'irregular/json_revivifier'
-require_relative 'irregular/present_only_irregular_mask'
 
 module Linguistics
   module Latin
@@ -15,7 +14,6 @@ module Linguistics
 
             def mutate!
               Revivifier.new(@verb, @deserializer.revivified_data_structure)
-              PresentOnlyIrregularMask.new(@verb).apply!
             end
           end
         end
