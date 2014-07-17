bf5d8924a0cbb0dcfa1a097fa0634b9dec9ddc01
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 07:13:18 -0700

    Collapse collapse Revivifier into Mutators::Irregular

--- lib/latinverb/tense_method_applicator/mutators/irregular.rb
+++ lib/latinverb/tense_method_applicator/mutators/irregular.rb
@@ -1,13 +1,42 @@
-require_relative 'irregular/json_deserializer'
-require_relative 'irregular/json_revivifier'
-require_relative 'irregular/present_only_irregular_mask'
-
 module Linguistics
   module Latin
     module Verb
       class LatinVerb
         module Mutators
           class Irregular
+            class Revivifier
+              def initialize(verb, structure)
+                @verb = verb
+                @structure = structure
+                build_tense_blocks!
+                build_infinitives!
+                build_participles!
+              end
+
+              private
+
+              def build_tense_blocks!
+                @structure['tense_blocks'].each_pair do |tense_block_name, serialized_tense_block|
+                  @verb.singleton_class.class_eval do
+                    define_method tense_block_name.to_sym do
+                      TenseBlock.new serialized_tense_block["data"], { :meaning => MEANINGS[tense_block_name.to_sym] }
+                    end
+                  end
+                end
+              end
+
+              def build_infinitives!
+                irregular_infinitives = @structure['infinitives']['data']
+                return if irregular_infinitives.nil?
+                InfinitivesBuilder.new(@verb, irregular_infinitives)
+              end
+
+              def build_participles!
+                irregular_participles = @structure['participles']['data']
+                return if irregular_participles.nil?
+                ParticiplesBuilder.new(@verb, irregular_participles)
+              end
+            end
             def initialize(verb)
               @verb = verb
               @deserializer = Deserializer.new(@verb)
