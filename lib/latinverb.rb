a3b44f7348e5417120bcd8cc4cab943654ea7393
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 04:32:30 -0700

    LatinVerb: respond to #querent: deleg to @tense_method_applicator

--- lib/latinverb.rb
+++ lib/latinverb.rb
@@ -32,6 +32,8 @@ module Linguistics
         def_delegator :@classifier, :dup, :classified_as
         def_delegator :@type_evaluator, :type, :verb_type
 
+        def_delegators :@tense_method_applicator, :querent
+
         attr_reader :original_string, :verb_methods, :classifier
 
         def initialize(data)
