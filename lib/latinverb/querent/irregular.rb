1162b1ad4ec5b592ae52c12d6dcdff77252e5c00
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 06:37:12 -0700

    Move conjugation-specific tense block calculation to LV from TenseMethodApplicator

---
+++ lib/latinverb/querent/irregular.rb
@@ -0,0 +1,17 @@
+module Linguistics
+  module Latin
+    module Verb
+      class LatinVerb
+        class IrregularQuerent < Querent
+          def active_voice_indicative_mood_present_tense
+            TenseBlock.new ["", "", @verb.original_string, "", "", ""]
+          end
+
+          def active_voice_indicative_mood_present_tense_third_person_singular_number
+            return @verb.active_voice_indicative_mood_present_tense[2]
+          end
+        end
+      end
+    end
+  end
+end
