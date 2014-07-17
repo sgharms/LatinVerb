8a676d9431f82cacaf76046d873a69bdf5b3319f
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 07:58:53 -0700

    Move Deponent "components" calculation out of querent_mutators/* -> QuerentMutators::Deponent

--- lib/latinverb/querent_mutators/deponent.rb
+++ lib/latinverb/querent_mutators/deponent.rb
@@ -1,7 +1,4 @@
 require_relative './deponent/tense_block_mutator'
-require_relative './deponent/imperative_mutator'
-require_relative './deponent/participle_mutator'
-require_relative './deponent/infinitive_mutator'
 
 module Linguistics
   module Latin
@@ -16,21 +13,69 @@ module Linguistics
               @verb = verb
               @querent = querent
               @proxyVerb = LatinVerb.new(proxy_string)
-              @mutators_classes = opts[:mutator_classes] || default_mutators
+              @mutators_classes = opts[:mutator_classes]
             end
 
             def mutate!
-              @mutators_classes.each{ |m| m.new(@verb, @querent, @proxyVerb) }
+              TenseBlockMutator.new(@verb, @querent, @proxyVerb).mutate!
+              bad
             end
 
             private
 
-            def default_mutators
-              [ TenseBlockMutator, ParticipleMutator, InfinitiveMutator ]
-            end
-
             def proxy_string
               DeponentStringDeriver.new(original_string).proxy_string
+            end
+
+            def bad
+                proxyVerb = @proxyVerb
+                # ParticipleMutator STUFF HERE:
+                @verb.instance_eval do
+                  @proxyVerb = proxyVerb
+
+                  def present_active_participle
+                    return @proxyVerb.present_active_participle
+                  end
+
+                  def future_active_participle
+                    return @proxyVerb.future_active_participle
+                  end
+
+                  def perfect_active_participle
+                    return @proxyVerb.perfect_passive_participle
+                  end
+
+                  def future_passive_participle
+                    return @proxyVerb.future_passive_participle
+                  end
+
+                  # Mask the supine
+                  def supine
+                    return @proxyVerb.supine
+                  end
+                end
+
+
+              # INFINITIVE MUTATOR STUFF HERE
+                @verb.instance_eval do
+                  @proxyVerb = proxyVerb
+
+                  def present_active_infinitive
+                    return @proxyVerb.send :present_passive_infinitive
+                  end
+
+                  def perfect_active_infinitive
+                    return @proxyVerb.send :perfect_passive_infinitive
+                  end
+
+                  def future_active_infinitive
+                    return @proxyVerb.send :future_active_infinitive
+                  end
+
+                  def present_passive_infinitive
+                    return NullTenseBlock.new
+                  end
+                end
             end
           end
         end
--- lib/latinverb/querent_mutators/deponent/infinitive_mutator.rb
+++ lib/latinverb/querent_mutators/deponent/infinitive_mutator.rb
@@ -14,26 +14,6 @@ module Linguistics
               end
 
               def mutate!
-                proxyVerb = @proxyVerb
-                @querent.instance_eval do
-                  @proxyVerb = proxyVerb
-
-                  def present_active_infinitive
-                    return @proxyVerb.send :present_passive_infinitive
-                  end
-
-                  def perfect_active_infinitive
-                    return @proxyVerb.send :perfect_passive_infinitive
-                  end
-
-                  def future_active_infinitive
-                    return @proxyVerb.send :future_active_infinitive
-                  end
-
-                  def present_passive_infinitive
-                    return NullTenseBlock.new
-                  end
-                end
               end
             end
           end
