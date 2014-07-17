42d5209bd5b16b267954aa9a9502f06094805a6f
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 04:43:37 -0700

    Add failing test of #querent on all Regular types

--- test/integration/third_io_conjugation_test.rb
+++ test/integration/third_io_conjugation_test.rb
@@ -184,7 +184,12 @@ class TestLatinVerbThirdIOConj < MiniTest::Unit::TestCase # :nodoc:
     assert_equal "capī",                       @aThirdIO.present_passive_infinitive
     assert_equal "captus, capta, captum esse", @aThirdIO.perfect_passive_infinitive
     assert_equal "captum īrī",                 @aThirdIO.future_passive_infinitive
+  end
 
+  def test_querent
+    assert @aThirdIO.querent, "Should find to a querent object"
+    assert_equal 6,  @aThirdIO.querent.active_voice_indicative_mood_present_tense.length
+    assert_equal "capiō", @aThirdIO.querent.active_voice_indicative_mood_present_tense_first_person_singular_number
   end
 
 end
