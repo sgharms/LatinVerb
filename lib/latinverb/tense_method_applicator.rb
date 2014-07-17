6690525f681c3e3ee3b7912ea507f42ba9d84d80
Steven G. Harms <steven.harms@gmail.com>
2014-07-17 04:55:27 -0700

    Introduce QuerentFactory

--- lib/latinverb/tense_method_applicator.rb
+++ lib/latinverb/tense_method_applicator.rb
@@ -1,3 +1,5 @@
+require_relative './tense_method_applicator/querent'
+require_relative './tense_method_applicator/querent_factory'
 require_relative './tense_method_applicator/defective_checker'
 require_relative './tense_method_applicator/deponent_string_deriver'
 require_relative './tense_method_applicator/perfect_tense_remover'
