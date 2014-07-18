e9d0d4a0fe5a05dea29d3c5e1cb74ac8af5a606e
Steven G. Harms <steven.harms@gmail.com>
2014-07-18 10:57:58 -0700

    Create LatinVerb::INFINITIVE_METHODS
    The presenter needs to have a place to ask a
    LatinVerb what its infinitive-y methods are.
    
    This should not change, so let's store it in a
    constant.

--- lib/latinverb/infinitves_factory.rb
+++ lib/latinverb/infinitves_factory.rb
@@ -2,6 +2,13 @@ module Linguistics
   module Latin
     module Verb
       class LatinVerb
+        INFINITIVE_METHODS = [
+          :future_active_infinitive,
+          :future_passive_infinitive,
+          :perfect_active_infinitive,
+          :perfect_passive_infinitive,
+          :present_passive_infinitive
+        ]
         class InfinitivesFactory < AbstractClusterFactory
           def irregular
             Linguistics::Latin::Verb::LatinVerb::IrregularInfinitivesRetriever.new(@verb.original_string).retrieve
