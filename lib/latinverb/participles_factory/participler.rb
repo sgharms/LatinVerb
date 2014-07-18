89cf55ce96c58817618912ce82ffc57c015256c8
Steven G. Harms <steven.harms@gmail.com>
2014-07-18 10:50:53 -0700

    Formatting

--- lib/latinverb/participles_factory/participler.rb
+++ lib/latinverb/participles_factory/participler.rb
@@ -13,15 +13,15 @@ module Linguistics
             @verb = verb
           end
 
-	  def participle_methods
-	    [
-	      :participle_methods,
-	      :present_active_participle,
-	      :future_active_participle,
-	      :perfect_passive_participle,
-	      :future_passive_participle
-	    ]
-	  end
+          def participle_methods
+            [
+              :participle_methods,
+              :present_active_participle,
+              :future_active_participle,
+              :perfect_passive_participle,
+              :future_passive_participle
+            ]
+          end
 
           def present_active_participle
             raise "Participial stem was nil" if  participial_stem.nil?
